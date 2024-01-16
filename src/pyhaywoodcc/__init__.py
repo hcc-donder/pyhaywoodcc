687570ec91d54225a10eef2e419edd41441b8aaa
David Onder <donder@haywood.edu>
2024-01-16 15:30:37 -0500

    Include load_data in __init__ file and include dist in repo

--- src/pyhaywoodcc/__init__.py
+++ src/pyhaywoodcc/__init__.py
@@ -3,8 +3,9 @@ from .ipeds import (
     fall_credential_seekers,
     fall_enrollment,
     ipeds_cohort,
+    load_data,
     term_enrollment,
 )
 from .utils import commas_to_mv, delim_to_mv, load_config, mv_to_commas, mv_to_delim
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
